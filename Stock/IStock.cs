7e829a777a0adfbeb36ae27fc35bd969082b52b9
Kiran Kumar K <Kiran.K@WinWire.com>
2019-07-01 10:56:05 +0530

    Committed

---
+++ Stock/IStock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Stock
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IStock" in both code and config file together.
+    [ServiceContract]
+    public interface IStock
+    {
+        [OperationContract]
+        Stock GetStock(string Symbol);
+    }
+
+    [DataContract]
+    public class Stock
+    {
+        [DataMember]
+        public string Symbol { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+        [DataMember]
+        public string Company { get; set; }
+        [DataMember]
+        public decimal Close { get; set; }
+    }
+}
---
+++ Stock/Stock.svc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Stock
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Stock" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Stock.svc or Stock.svc.cs at the Solution Explorer and start debugging.
+    public class Stocks : IStock
+    {
+        #region IStock Members
+        public Stock GetStock(string Symbol)
+        {
+            Stock st = null;
+            switch (Symbol.ToUpper())
+            {
+                case "GOOG":
+                    st = new Stock
+                    {
+                        Symbol = Symbol,
+                        Date = DateTime.Now,
+                        Company = "Google Inc.",
+                        Close = 495
+                    };
+                    break;
+                case "MSFT":
+                    st = new Stock
+                    {
+                        Symbol = Symbol,
+                        Date = DateTime.Now,
+                        Company = "Microsoft Corporation",
+                        Close = 25
+                    };
+                    break;
+                case "YHOO":
+                    st = new Stock
+                    {
+                        Symbol = Symbol,
+                        Date = DateTime.Now,
+                        Company = "Yahoo! Inc.",
+                        Close = 17
+                    };
+                    break;
+                case "AMZN":
+                    st = new Stock
+                    {
+                        Symbol = Symbol,
+                        Date = DateTime.Now,
+                        Company = "Amazon.com, Inc.",
+                        Close = 92
+                    };
+                    break;
+            }
+            return st;
+        }
+        #endregion
+    }
+}
